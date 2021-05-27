62cd02f7d75f244db889008cf0d238d131600b64
Shubenkovll <79252013+Shubenkovll@users.noreply.github.com>
2021-05-27 12:26:49 +0300

    Add files via upload

---
+++ copy_files_and_search_max.py
@@ -0,0 +1,24 @@
+import os, shutil
+
+def copy_file_max_size():
+    path = (input('Enter the directory: '))
+    new_path = (input('Enter the file copy directory: '))
+    os.chdir(path)
+    i = 0
+
+    print('Copied files:')
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            print(os.path.join(root, file))
+            full_file_name = os.path.join(root, file)
+            shutil.copy(full_file_name, new_path)
+            i += 1
+        
+            maximum_file_size = 0
+            if os.path.getsize(full_file_name) > maximum_file_size:
+                maximum_file_size = round(os.path.getsize(full_file_name)  * 9.537*10**(-7),1)
+                file_name = full_file_name
+    return {
+    print(f'Files copied:  {i}\n', f'Largest file:  {file_name}\n', 
+    f'Size:  {maximum_file_size}' + ' MB')
+    }
