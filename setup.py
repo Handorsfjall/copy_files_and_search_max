c54dde4ba263e2e611a3c51c7c54c4808eb763b7
Shubenkovll <79252013+Shubenkovll@users.noreply.github.com>
2021-06-14 18:35:49 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+import copy_file_and_search_max
+
+with open('README.md') as fr:
+    long_description = fr.read()
+
+setuptools.setup(
+    name='copy_file_and_search_max',
+    version=copy_file_and_search_max.__version__,
+    author='Shubenkov A.Y.',
+    author_mail='shubenkov@tuta.io',
+    desctiption='Copying files from a directory and finding the largest one',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Shubenkovll/copy_file_and_search_max',
+    packages=setuptools.find_packages(),
+    install_requires=[
+
+
+    ],
+    test_suite='tests',
+    python_requires='>=3.9',
+    platforms=["any"]
+)
+
+
+
+
+
+
+
+
+
+)
