b52083216d698d5211dac1a0fce5e333013bac11
Shubenkovll <79252013+Shubenkovll@users.noreply.github.com>
2021-06-14 23:21:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md') as fr:
     long_description = fr.read()
 
 setuptools.setup(
-    name='copy_file_and_search_max',
+    name='copy_files_and_search_max',
     version=copy_file_and_search_max.__version__,
     author='Shubenkov A.Y.',
     author_mail='shubenkov@tuta.io',
     desctiption='Copying files from a directory and finding the largest one',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/Shubenkovll/copy_file_and_search_max',
+    url='https://github.com/Shubenkovll/copy_files_and_search_max',
     packages=setuptools.find_packages(),
     install_requires=[
 
